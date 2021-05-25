9c21fe99fc33d1437fb9dfce45d862c314fccc79
Robin Dreher <robin.dreher@web.de>
2021-05-25 19:13:48 +0200

    slightly improved version of the script I used to generate tables and plots for the paper (tables still need some formatting afterwards)

---
+++ R/latex-export.R
@@ -0,0 +1,33 @@
+library(xtable)
+
+set.seed(1)
+source("R/results.R")
+
+methods=c(GSK_METHODS, "bayesian_t_test")
+
+errors = calculate_error_rates(results, methods)
+impact_power = calculate_impact_of_power(results, methods)
+impact_mpsd = calculate_impact_of_MPSD(results, methods)
+impact_power_on_fdr = calculate_impact_of_power_on_false_discovery_rate(results, methods)
+impact_power_on_for = calculate_impact_of_power_on_false_omission_rate(results, methods)
+impact_on_fdr_for = rbind(impact_power_on_fdr, impact_power_on_for)
+for (method in methods) {
+  impact_power[[method]] = impact_power[[method]]*100  
+  impact_mpsd[[method]] = impact_mpsd[[method]]*100
+  impact_on_fdr_for[[method]] = impact_on_fdr_for[[method]]*100
+  
+  if (method %in% names(METHOD_NAMES)) {
+    rownames(errors)[rownames(errors) == method] = METHOD_NAMES[[method]]
+    colnames(impact_power)[colnames(impact_power) == method] = METHOD_NAMES[[method]]
+    colnames(impact_mpsd)[colnames(impact_mpsd) == method] = METHOD_NAMES[[method]]
+    colnames(impact_on_fdr_for)[colnames(impact_on_fdr_for) == method] = METHOD_NAMES[[method]]
+  }
+}
+
+xtable(errors*100, digits=1, align=strrep("c", ncol(errors)+1), label="tab:error_rates")
+xtable(impact_power, digits=1, align=strrep("c", ncol(impact_power)+1), label="tab:power_comparison")
+xtable(impact_mpsd, digits=1, align=strrep("c", ncol(impact_mpsd)+1), label="tab:relativempsdcomparison")
+xtable(impact_on_fdr_for, digits=1, align=strrep("c", ncol(impact_on_fdr_for)+1), label="tab:fdr-for")
+
+plot_impact_of_power(results, methods)
+plot_impact_of_MPSD(results, methods)
