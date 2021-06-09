a1fdd7d55d7ba5f107a84000eef2d4a317242e2a
Robin Dreher <robin.dreher@web.de>
2021-06-09 19:40:05 +0200

    add % to tables

--- R/latex-export.R
+++ R/latex-export.R
@@ -1,6 +1,6 @@
 library(xtable)
 
-set.seed(1)
+#set.seed(1)
 source("R/results.R")
 
 methods=c(GSK_METHODS, "thick_t_test")
@@ -13,10 +13,10 @@ impact_power_on_for = calculate_impact_of_power_on_false_omission_rate(results, 
 impact_on_fdr_for = rbind(impact_power_on_fdr, impact_power_on_for)
 impact_on_fdr_for_new = calculate_impact_of_power_on_false_discovery_and_omission_rate(results, methods)
 for (method in methods) {
-  impact_power[[method]] = impact_power[[method]]*100  
-  impact_mpsd[[method]] = impact_mpsd[[method]]*100
-  impact_on_fdr_for[[method]] = impact_on_fdr_for[[method]]*100
-  impact_on_fdr_for_new[[method]] = impact_on_fdr_for_new[[method]]*100
+  impact_power[[method]] = paste(round(impact_power[[method]]*100, 1), "%", sep="")
+  impact_mpsd[[method]] = paste(round(impact_mpsd[[method]]*100, 1), "%", sep="")
+  impact_on_fdr_for[[method]] = paste(round(impact_on_fdr_for[[method]]*100, 1), "%", sep="")
+  impact_on_fdr_for_new[[method]] = paste(round(impact_on_fdr_for_new[[method]]*100, 1), "%", sep="")
   
   if (method %in% names(METHOD_NAMES)) {
     colnames(errors)[colnames(errors) == method] = METHOD_NAMES[[method]]
