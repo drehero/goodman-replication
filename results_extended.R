8535c0bf3bf50e14e35a76950bb75c0c73329908
Arne Kramer-Sunderbrink <herr-kramer-sunderbrink@t-online.de>
2021-10-07 10:57:51 +0200

    corrected mistake in the implementation of the truncated normal density

--- results_extended.R
+++ results_extended.R
@@ -6,6 +6,6 @@ calculate_error_rates(results_norm)
 calculate_error_rates(results_point)
 
 plot_roc_curve(results_alphas)
-plot_roc_curve(results_alphas, METHODS, detailed=TRUE)
+plot_roc_curve(results_alphas, METHODS, detailed=TRUE)  # todo: vllt könnte man distance only noch als punkt hinzufügen
 
-plot_alpha_curve(results_alphas, METHODS, detailed=TRUE)
+plot_alpha_curve(results_alphas, METHODS, detailed=TRUE)  # todo vllt könnte man distance only noch als horrizontale linie hinzufügen
